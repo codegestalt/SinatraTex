8085b1877a3d2c24913a141c3acef93eada7e2e2
Rodrigo Haenggi <rodrigo@codegestalt.com>
2013-12-09 14:57:06 +0100

    create tmp directory structure if it does not exist

--- settings.rb
+++ settings.rb
@@ -1,19 +1,25 @@
 ### Variables which are good to tweak ###
+require 'fileutils'
 
 # Main Path
 output_temp_dir = "/tmp/tex2png"
+Dir.mkdir(output_temp_dir) unless File.exists?(output_temp_dir)
 
 # Temporary dvi file
 TEMP_DVI = "%s/dvi" % output_temp_dir
+Dir.mkdir(TEMP_DVI) unless File.exists?(TEMP_DVI)
 
 # Temporary png file
 TEMP_PNG = "%s/png" % output_temp_dir
+Dir.mkdir(TEMP_PNG) unless File.exists?(TEMP_PNG)
 
 # Temporary tex file
 TEMP_TEX = "%s/tex" % output_temp_dir
+Dir.mkdir(TEMP_TEX) unless File.exists?(TEMP_TEX)
 
 # Loggin access and errors
 LOGFILE = "%s/log" % output_temp_dir
+Dir.mkdir(LOGFILE) unless File.exists?(LOGFILE)
 
 TEX_BLACKLIST = ["\\def", "\\let", "\\futurelet",
         "\\newcommand", "\\renewcommand", "\\else", "\\fi", "\\write",
