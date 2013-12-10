af0a884f7c800dfae9a79ee5e6a67a1e7f977b32
Daniel Puglisi <pulleasy@gmail.com>
2013-12-10 16:10:38 +0100

    use FileUtils to create subdirectories

--- settings.rb
+++ settings.rb
@@ -3,26 +3,27 @@ require 'fileutils'
 
 # Main Path
 output_temp_dir = "./tmp/tex2png"
-Dir.mkdir(output_temp_dir) unless File.exists?(output_temp_dir)
+FileUtils.mkdir_p(output_temp_dir) unless File.exists?(output_temp_dir)
 
-# Temporary dvi file
+# Temporary pdf file
 TEMP_PDF = "%s/pdf" % output_temp_dir
+FileUtils.mkdir_p(TEMP_PDF) unless File.exists?(TEMP_PDF)
 
 # Temporary dvi file
 TEMP_DVI = "%s/dvi" % output_temp_dir
-Dir.mkdir(TEMP_DVI) unless File.exists?(TEMP_DVI)
+FileUtils.mkdir_p(TEMP_DVI) unless File.exists?(TEMP_DVI)
 
 # Temporary png file
 TEMP_PNG = "%s/png" % output_temp_dir
-Dir.mkdir(TEMP_PNG) unless File.exists?(TEMP_PNG)
+FileUtils.mkdir_p(TEMP_PNG) unless File.exists?(TEMP_PNG)
 
 # Temporary tex file
 TEMP_TEX = "%s/tex" % output_temp_dir
-Dir.mkdir(TEMP_TEX) unless File.exists?(TEMP_TEX)
+FileUtils.mkdir_p(TEMP_TEX) unless File.exists?(TEMP_TEX)
 
 # Loggin access and errors
 LOGFILE = "%s/log" % output_temp_dir
-Dir.mkdir(LOGFILE) unless File.exists?(LOGFILE)
+FileUtils.mkdir_p(LOGFILE) unless File.exists?(LOGFILE)
 
 TEX_BLACKLIST = ["\\def", "\\let", "\\futurelet",
         "\\newcommand", "\\renewcommand", "\\else", "\\fi", "\\write",
